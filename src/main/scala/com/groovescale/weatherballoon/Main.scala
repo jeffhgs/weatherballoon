527c57a093a28c804add9e2345bc4df7acc6a579
Jeff Henrikson <jeff@groovescale.com>
2019-03-26 14:46:50 -0700

    implement: -- separator; usage

--- src/main/scala/com/groovescale/weatherballoon/Main.scala
+++ src/main/scala/com/groovescale/weatherballoon/Main.scala
@@ -5,9 +5,22 @@ import org.slf4j.LoggerFactory
 object Main {
   val log = LoggerFactory.getLogger(Main.getClass())
 
+  def usage() = {
+    System.err.println(
+      """
+        |usage:
+        |  weatherballoon.sh -- <command to run remotely>
+      """.stripMargin)
+  }
+
   def main(args: Array[String]): Unit = {
-    val cmd = args
-    val cfg = ConfigUtil.getCfg()
-    AwsProvisioner.runProvisioned(cmd, cfg)
+    val iCommand = args.indexOf("--")
+    if(iCommand >= 0 && (args.length - (iCommand+1) >= 1)) {
+      val cmd = args.drop(iCommand+1)
+      val cfg = ConfigUtil.getCfg()
+      AwsProvisioner.runProvisioned(cmd, cfg)
+    } else {
+      usage()
+    }
   }
 }
