82e927bb2bebaaabeecdab72c7efd840105af7d9
Jeff Henrikson <jeff@groovescale.com>
2019-03-26 14:46:33 -0700

    implement: install rclone

--- src/main/resources/file/install_deps.sh
+++ src/main/resources/file/install_deps.sh
@@ -2,6 +2,7 @@ apt-get update
 apt-get install -y \
     ec2-api-tools \
     jq
+curl https://rclone.org/install.sh | bash
 apt-get install -y \
     openjdk-8-jre-headless \
     openjdk-8-jdk-headless
