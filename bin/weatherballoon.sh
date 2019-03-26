3f5298b40acd12474046e93e437be11f12191732
Jeff Henrikson <jeff@groovescale.com>
2019-03-26 14:46:50 -0700

    implement: generate build/dist.zip
    amend: implement: generate build/dist.zip

--- bin/weatherballoon.sh
+++ bin/weatherballoon.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-adirBase="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+adirBase="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-java -cp "$adirBase/reports/libs/bcprov-jdk15on-1.56.jar:$adirBase/build/libs/weatherballoon_shadow.jar" com.groovescale.weatherballoon.Main "$@"
+java -cp "$adirBase/bcprov-jdk15on-1.56.jar:$adirBase/weatherballoon_shadow.jar" com.groovescale.weatherballoon.Main "$@"
+
