eb9bdb45fcd231c7e3f7ceca50840969b114eff5
Jeff Henrikson <jeff@groovescale.com>
2019-03-26 14:46:32 -0700

    refactor: break lines

--- src/main/resources/file/install_deps.sh
+++ src/main/resources/file/install_deps.sh
@@ -1,3 +1,7 @@
 apt-get update
-apt-get install -y ec2-api-tools jq
-apt-get install -y openjdk-8-jre-headless openjdk-8-jdk-headless
+apt-get install -y \
+    ec2-api-tools \
+    jq
+apt-get install -y \
+    openjdk-8-jre-headless \
+    openjdk-8-jdk-headless
