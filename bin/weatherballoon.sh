bcd77595ad3c86afcbee7c0d9089258348b7ac06
Jeff Henrikson <jeff@groovescale.com>
2019-03-26 14:46:36 -0700

    implement: wrapper script and build script to get command line arguments to work

---
+++ bin/weatherballoon.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+adirBase="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+
+java -cp "$adirBase/reports/libs/bcprov-jdk15on-1.56.jar:$adirBase/build/libs/weatherballoon_shadow.jar" com.groovescale.Run1 "$@"
